a78df80dbc2573d9189aa756895df88eb7b0a0f6
antaris1979 <costas.kontopoulos@outlook.com>
2022-10-28 19:17:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed
+        
         </a>
       </header>
     </div>
